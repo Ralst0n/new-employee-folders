17d863b4b785f662885bb35cbeb5cccceb2941b1
Ralst0n <pghRalston@gmail.com>
2017-11-07 08:33:22 -0500

    add newResumeFolder app

---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace newResumefolder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            notifyIcon1.Visible = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!(EmpNumberBox.Text == String.Empty || int.TryParse(EmpNumberBox.Text, out int n)))
+            {
+                MessageBox.Show("Employee Number can only include digits");
+                EmpNumberBox.Focus();
+                return;
+            }
+            if (!comboBox1.Items.Contains(comboBox1.Text))
+            {
+                MessageBox.Show("Invalid office selection");
+                comboBox1.Focus();
+                return;
+            }
+            NewDirectory createdDirectory = new NewDirectory(FirstNameBox.Text, LastNameBox.Text, comboBox1.SelectedItem.ToString(),EmpNumberBox.Text);
+
+            if (createdDirectory.CreateDirectories())
+                MessageBox.Show($"Successfully created folders for {FirstNameBox.Text} {LastNameBox.Text}");
+            else
+                MessageBox.Show($"Folders for {FirstNameBox.Text} {LastNameBox.Text} already exist");
+            ClearForm();
+
+        }
+        private void ClearForm()
+        {
+            FirstNameBox.Text = String.Empty;
+            LastNameBox.Text = String.Empty;
+            comboBox1.SelectedItem = null;
+            EmpNumberBox.Text = String.Empty;
+            SendKeys.Send("{TAB}");
+        }
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            Invoke(new Action(() => { MessageBox.Show("Directory Builder v1.0\nAuthor: Ralston Lawson\n\u00A9 2017", "About"); }));
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Invoke(new Action(() => { MessageBox.Show("Directory Builder v1.0\nAuthor: Ralston Lawson\n\u00A9 2017", "About"); }));
+        }
+
+    }
+}
---
+++ NewDirectory.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace newResumefolder
+{
+    class NewDirectory
+    {
+        public NewDirectory(string firstName, string lastName, string location, string employeeNumber = "")
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.location = location;
+            this.employeeNumber = employeeNumber;
+            if (location == "Pittsburgh")
+                employeePath = @"P:\012 - EMPLOYMENT\PERSONNEL FILE\ACTIVE EMPLOYEES\PGH\";
+            else
+                employeePath = @"P:\012 - EMPLOYMENT\PERSONNEL FILE\ACTIVE EMPLOYEES\KOP\";
+        }
+
+        private string firstName;
+        private string lastName;
+        private string location;
+        private string employeeNumber;
+        private string resumePath =  @"P:\012 - EMPLOYMENT\RESUMES\Resumes\";
+        private string employeePath;
+
+       private bool CreatePersonnel()
+        {
+            string[] subfolders = new string[] { "Certifications", "Correspondence", "Emergency Contact", "New Hire Documents", "Status Change", "Wage Acknowledgement" };
+            bool changes = false;
+            string personnelFolder = $"{lastName.ToUpper()}, {firstName.ToUpper()}";
+            if (!Directory.Exists(employeePath + personnelFolder))
+                Directory.CreateDirectory(employeePath + personnelFolder);
+
+            foreach (string folder in subfolders)
+            {
+                //if the employee folder is missing one of the subfolders, then add it.
+                Console.WriteLine(employeePath + personnelFolder + @"\" + folder);
+                if (!Directory.Exists(employeePath + personnelFolder + @"\" + folder))
+                {
+                    Directory.CreateDirectory(employeePath + personnelFolder + @"\" + folder);
+                    changes = true;
+                }
+            }
+            return changes;
+
+        }
+
+        private bool CreateResume()
+        {
+            string[] subfolders = new string[] { "Current Certifications", "Expired Certifications", "Support Documentation"};
+            string resumeFolder = $"{ProperName(lastName)}-{ProperName(firstName)} {employeeNumber}";
+            bool changes = false;
+            if (!Directory.Exists(resumePath + resumeFolder)) 
+                Directory.CreateDirectory(resumePath + resumeFolder);
+            foreach (string folder in subfolders)
+            {
+                if(!Directory.Exists(resumePath + resumeFolder + @"\" + folder))
+                {
+                    Directory.CreateDirectory(resumePath + resumeFolder + @"\" + folder);
+                    changes = true;
+                }
+            }
+            return changes;
+        }
+
+        public bool CreateDirectories()
+        {
+            bool changes = false;
+            if (CreateResume())
+                changes = true;
+            if (CreatePersonnel())
+                changes = true;
+            return changes;
+        }
+        private string ProperName(string name)
+        {
+            return name.First().ToString().ToUpper() + name.Substring(1);
+        }
+    }
+}
